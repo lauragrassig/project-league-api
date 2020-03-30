d82202dbff73208ccb48655b85df0c5ee97cfa97
lauragrassig <laura_grassig@outlook.com>
2020-03-30 17:30:33 -0300

    Router + Cards + Select Language

--- src/Routes.js
+++ src/Routes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {  BrowserRouter, Route } from 'react-router-dom';
 
 import SelectLanguage from './view/SelectLanguage';
-// import Main from './pages/Main';
+import Cards from './view/Cards';
 
 export default function Routes () {
 
   return (
     <BrowserRouter>
       <Route path="/" exact component={SelectLanguage}/>
-      {/* <Route path="/main" component={Main}/> */}
+      <Route path="/cards" component={Cards}/>
     </BrowserRouter>
   );
 }
--- src/view/SelectLanguage.js
+++ src/view/SelectLanguage.js
@@ -1,9 +1,78 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <h3>abriu</h3>
-  );
+const LanguageAPI = "http://demo3139755.mockable.io/leagueLanguages";
+
+class SelectLanguage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      languages: [],
+      isLoading: true,
+      error: null,
+      error2: null,
+      SelectedLanguage: ''
+    };
+  }
+
+  onChange = event => {
+    this.setState({ SelectedLanguage: event.target.value })
+  }
+
+  returnLanguage = event => {
+    this.setState({ error: false })
+  }
+  
+  onSearch = event => {
+    event.preventDefault();
+    const { SelectedLanguage } = this.state;
+    
+    //*~ Salvar no localStorage + Verificar se há valor + Alterar página
+    if ( SelectedLanguage === '' || SelectedLanguage == null){
+      this.setState({ error: true })
+    } else {
+      this.props.history.push({pathname: '/Cards', state: {SelectedLanguage}});
+    }
+  }
+
+  componentDidMount() {
+    fetch(LanguageAPI)
+    .then( res => {
+      if(res.ok) {
+        return res.json();
+      } else {
+        throw new Error ('Algo errado');
+      }
+    })
+    .then(result => this.setState ({ languages: result.languages, isLoading: false}))
+    .catch(error => this.setState ({ error, isLoading: false }));
+  }
+
+  render () {
+
+    const { isLoading, error } = this.state;
+    if (isLoading) { return <p>Loading ...</p>; }
+
+    if (error) { return <div>
+      <p>{error.message}</p>
+      <p>Por favor preencha novamente</p>
+      <button onClick={this.returnLanguage}>teste</button>
+    </div> } 
+
+    return (
+      <div className="wrapper--language">
+        <form onSubmit={this.onSearch}>
+          <select onChange={this.onChange}>
+            {this.state.languages.map((language, key) => (
+            <option key={key} value={language.value}>{language.name}</option>
+            ))}
+          </select>
+          <button className="language--button pulse" type="submit">
+            <span>Acessar</span>
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default SelectLanguage;
